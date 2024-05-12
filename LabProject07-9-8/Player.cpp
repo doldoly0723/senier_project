da764ec22850fc329c370fe9cc2aeb4675e355b8
Kim Tae Hyun <akdl6632@tukorea.ac.kr>
2024-05-13 00:39:08 +0900

    최대 속도 조정
    가속을 안하게 하면 T-Pos 로 움직임
    최대 속도를 낮춰서 가속이 안붙는 것 처럼 보이게 함

--- LabProject07-9-8/Player.cpp
+++ LabProject07-9-8/Player.cpp
@@ -467,8 +467,10 @@ CCamera *CTerrainPlayer::ChangeCamera(DWORD nNewCameraMode, float fTimeElapsed)
 		case THIRD_PERSON_CAMERA:
 			SetFriction(250.0f);
 			SetGravity(XMFLOAT3(0.0f, -250.0f, 0.0f));
-			SetMaxVelocityXZ(300.0f);
-			SetMaxVelocityY(400.0f);
+			//SetMaxVelocityXZ(300.0f);
+			//SetMaxVelocityY(400.0f);
+			SetMaxVelocityXZ(70.0f);
+			SetMaxVelocityY(70.0f);
 			m_pCamera = OnChangeCamera(THIRD_PERSON_CAMERA, nCurrentCameraMode);
 			m_pCamera->SetTimeLag(0.25f);
 			m_pCamera->SetOffset(XMFLOAT3(0.0f, 20.0f, -20.0f));
