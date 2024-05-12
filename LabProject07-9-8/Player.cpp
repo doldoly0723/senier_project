4d746fe34d66fae0de3b31cac03a0f1dbb430dbc
Kim Mun Su <doldoly0723@naver.com>
2024-05-13 04:31:42 +0900

    맵 오브젝트 추가 및 위치 수정

--- LabProject07-9-8/Player.cpp
+++ LabProject07-9-8/Player.cpp
@@ -424,7 +424,7 @@ CTerrainPlayer::CTerrainPlayer(ID3D12Device *pd3dDevice, ID3D12GraphicsCommandLi
 	SetCameraUpdatedContext(pContext);
 
 	CHeightMapTerrain *pTerrain = (CHeightMapTerrain *)pContext;
-	SetPosition(XMFLOAT3(310.0f, pTerrain->GetHeight(310.0f, 590.0f), 590.0f));
+	SetPosition(XMFLOAT3(1310.0f, pTerrain->GetHeight(310.0f, 590.0f), 1590.0f));
 	SetScale(XMFLOAT3(10.0f, 10.0f, 10.0f));
 
 	if (pAngrybotModel) delete pAngrybotModel;
